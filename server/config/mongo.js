4b5ace54acb53a09f6255d2daf30bd16cd788529
Quest Henkart <qhenkart@Quests-MacBook-Pro.local>
2015-09-29 17:25:36 -0700

    now using mongo server instance

--- server/config/mongo.js
+++ server/config/mongo.js
@@ -5,4 +5,4 @@ var Collection = mongodb.Collection;
 Promise.promisifyAll(MongoClient);
 Promise.promisifyAll(Collection.prototype);
 
-module.exports = MongoClient.connectAsync('mongodb://localhost/pantasy');
+module.exports = MongoClient.connectAsync('mongodb://dummy:a12345667@ec2-52-26-139-62.us-west-2.compute.amazonaws.com:27017/dummyDB');
